11966242dfcd6e751d6713a8dc0ec4c43de52215
David Hallberg Jönsson <d@vidhallberg.se>
2014-11-15 15:41:03 +0100

    Cleaned moviehandler.js

--- moviehandler.js
+++ moviehandler.js
@@ -1,25 +1,15 @@
 function fetchMovies(user, method, callback) {
     var baseURL = 'http://api.trakt.tv';
     var apikey = 'eca8a8e86968052661e1027d3eaeb444';
-
     var extra = '';
     var url = baseURL + '/' + method + '/' + apikey + '/' + user + extra + '?callback=?';
 
     $.getJSON(url).success(function(data) {
-        //$('#source_selector').hide();
         movies = data;
-        //chooseMovie();
         callback('success');
     }).error(function() {
-        //$('.source_error').show();
         callback('error');
-    })//.complete(function() {
-        //$('#loading_image').hide();
-        //if(!$('#source_selector').is(':visible')) {
-            //$('#overlay').hide();
-        //}
-    //});
-}
+    })}
 
 function setRatings(id) {
     var baseurl = 'http://www.omdbapi.com/?i=';
