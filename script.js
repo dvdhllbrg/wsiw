99c4d5b9b5c75eaf9951ab08878df7590d51bfe5
David Hallberg Jönsson <d@vidhallberg.se>
2014-11-15 21:36:25 +0100

    Combine uihandler.js and moviehandler.js to script.js because, honestly, it was a little silly to split it up.

--- script.js
+++ script.js
@@ -97,3 +97,26 @@ function showSourceSelector() {
     $('#select_source_button').show();
     $('#overlay').show();
 }
+
+function fetchMovies(user, method, extra, callback) {
+    var baseURL = 'http://api.trakt.tv';
+    var apikey = 'eca8a8e86968052661e1027d3eaeb444';
+    var url = baseURL + '/' + method + '/' + apikey + '/' + user + extra + '?callback=?';
+
+    $.getJSON(url).success(function(data) {
+        movies = data;
+        callback('success');
+    }).error(function() {
+        callback('error');
+    })
+}
+
+function getRatings(id, callback) {
+    var baseurl = 'http://www.omdbapi.com/?i=';
+    var tomatoes = '&tomatoes=true';
+    var url = baseurl + id + tomatoes;
+
+    $.getJSON(url, function(data) {
+        callback(data.imdbRating, data.tomatoRating);
+    });
+}
