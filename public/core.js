f558524bf44f503fc380b7197e630ef2361612c2
David Hallberg Jönsson <d@vidhallberg.se>
2014-12-01 21:47:52 +0100

    Added user choosing

--- public/core.js
+++ public/core.js
@@ -4,6 +4,7 @@ function mainController($scope, $http) {
     $scope.source = '';
     $scope.movies = null;
     $scope.movie = null;
+    $scope.loading = false;
 
     $http.get('/api/movies')
         .success(function(data) {
@@ -18,26 +19,28 @@ function mainController($scope, $http) {
     $scope.getMovies = function() {
         var baseURL = 'http://api.trakt.tv';
         var method = '';
+        var user = '';
         var extra = '';
         switch ($scope.source) {
             case 'watchlist':
             method = 'user/watchlist/movies.json';
+            user = $scope.wl_user;
             break;
             case 'collection':
                 method = 'user/library/movies/collection.json';
+                user = $scope.c_user;
                 extra = '/extended';
                 break;
             case 'trending':
                 method = 'movies/trending.json';
         }
         var apikey = 'eca8a8e86968052661e1027d3eaeb444';
-        var user = 'iamhj';
+        var user = typeof user === 'undefined' || user == '' ? 'iamhj' : user;
         var url = baseURL + '/' + method + '/' + apikey + '/' + user + extra + '?callback=JSON_CALLBACK';
         $http.jsonp(url)
             .success(function(movies) {
                 $scope.movies = movies;
-                console.log(movies);
-                console.log($scope.movies);
+                $scope.loading = true;
                 $scope.chooseMovie();
             })
             .error(function(data) {
@@ -47,6 +50,7 @@ function mainController($scope, $http) {
 
     $scope.chooseMovie = function() {
        $scope.movie = $scope.movies[Math.floor(Math.random()*$scope.movies.length)];
+       $scope.loading = false;
     }
 
     $scope.createMovie = function() {
