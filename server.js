f617e55b7822fa8dac1fe2c9b88317b9a1e20a4f
David Hallberg Jönsson <d@vidhallberg.se>
2014-12-04 11:38:40 +0100

    Removed unused dependencies

--- server.js
+++ server.js
@@ -2,25 +2,12 @@ var express = require('express');
 var app = express();
 
 var mongoose = require('mongoose');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 var compress = require('compression');
 
 mongoose.connect('mongodb://localhost/wsiw');
 
 app.use(express.static(__dirname + '/public'));
 app.use(compress());
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ 'extended' : 'true' }));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(methodOverride());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 var movieSchema = mongoose.Schema( {
     movie: Object
