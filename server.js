8bdda29be38255504e32fa2404f43bbdf1e21629
David Hallberg Jönsson <d@vidhallberg.se>
2014-12-06 20:50:30 +0100

    Or just, you know, do it the way that works.

--- server.js
+++ server.js
@@ -9,25 +9,38 @@ mongoose.connect('mongodb://localhost/wsiw');
 app.use(express.static(__dirname + '/public'));
 app.use(compress());
 
-var movieSchema = mongoose.Schema( {
+var imdbSchema = mongoose.Schema( {
         movie: Object
     }, {
         collection : 'top250'
     });
+var imdbMovie = mongoose.model('imdbMovie', imdbSchema);
 
-var Movie = mongoose.model('Movie', movieSchema);
+var rtSchema = mongoose.Schema( {
+        movie: Object
+    }, {
+        collection : 'rt'
+    });
+var rtMovie = mongoose.model('rtMovie', rtSchema);
+
 
 // Routes
 app.get('/api/movies/:source', function(req, res) {
-    movieSchema.set('collection', req.params.source);
-    Movie.find(function(err, movies) {
+    if(req.params.source == 'top250') {
+        imdbMovie.find(function(err, movies) {
             if(err) {
                 res.send(err);
             }
-        res.json(movies);
-    });
-
-    delete Movie;
+            res.json(movies);
+        });
+    } else if(req.params.source == 'rt') {
+        rtMovie.find(function(err, movies) {
+            if(err) {
+                res.send(err);
+            }
+            res.json(movies);
+        });
+    }
 });
 
 // Application
